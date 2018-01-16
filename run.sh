75bc93c75783111fbc823d8653d60f78fdf739b4
Rohan Doshi <rkdoshi@princeton.edu>
2018-01-16 14:48:57 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 train.py --num_routing_iterations=5 --dataset='mnist' --gpu=0 &
-python3 train.py --num_routing_iterations=6 --dataset='mnist' --gpu=0 &
-python3 train.py --num_routing_iterations=7 --dataset='mnist' --gpu=1 &
-python3 train.py --num_routing_iterations=8 --dataset='mnist' --gpu=1 &
-
-
